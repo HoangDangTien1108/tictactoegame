39bd8e439783f256145d295c9191838e0ff7975f
tiendp <hoangdangtien1108@gmail.com>
2023-11-07 16:13:37 +0700

    complete code

--- src/components/tictactoe/Board.js
+++ src/components/tictactoe/Board.js
@@ -4,7 +4,7 @@ import Cell from "./Cell";
 export default function Board(props) {
   return (
     <>
-      <div className="sm:w-[500px] h-[500px] w-[100%] border border-gray-300 mx-auto sm:mt-[50px] mt-[10px] grid grid-cols-3 grid-rows-3 mb-[20px]">
+      <div className="sm:w-[500px] sm:h-[500px] h-[400px] w-[100%] border border-gray-300 mx-auto sm:mt-[50px] mt-[10px] grid grid-cols-3 grid-rows-3 mb-[20px]">
         {props.cells.map((item, index) => {
           return (
             <Cell
